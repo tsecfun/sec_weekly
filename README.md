6a1adac5bcd2f2a4c2f6f72d9ce9c8cb1141535d
mclass <mclass@mclassMacBook-Pro.local>
2023-02-01 23:38:00 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,7 @@
+# 网络安全笔记
+
+
+
+
+
+模板来自：https://github.com/tw93/weekly
---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export const SITE = {
+  title: '网络安全周刊',
+  author: 'mclass',
+  description: '记录安全工程师tsec职业生涯的每一步，欢迎关注',
+  keywords: 'tsec,Weekly,Blog,网络安全,信息安全,安全运营,应急响应',
+  icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
+  pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
+  homePage: 'https://b.tsec.wiki',
+  blogPage: 'https://tsec.wiki',
+  repo: ''
+};
+
