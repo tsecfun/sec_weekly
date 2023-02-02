6386a576d45e57c8a24e2acb3a5ac8d75d69c884
tsecfun <fefree666@163.com>
2023-02-02 11:16:42 +0800

    add public

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: 'mclass',
   description: '记录安全工程师tsec所见所闻，欢迎关注',
   keywords: 'tsec,Blog,网络安全,信息安全,安全运营,应急响应',
-  icon: 'http://pic.hackmyhome.top/i/202302020119787.png',
+  icon: '/public/icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
   homePage: 'https://b.tsec.wiki',
   blogPage: 'https://tsec.wiki',
