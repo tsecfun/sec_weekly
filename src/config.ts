a6c9b5d5a5d3c75559450375181f9de621549559
mclass <mclass@mclassMacBook-Pro.local>
2023-02-02 10:04:00 +0800

    更新

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  title: '网络安全周刊',
+  title: '网络安全笔记',
   author: 'mclass',
-  description: '记录安全工程师tsec职业生涯的每一步，欢迎关注',
-  keywords: 'tsec,Weekly,Blog,网络安全,信息安全,安全运营,应急响应',
-  icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
+  description: '记录安全工程师tsec所见所闻，欢迎关注',
+  keywords: 'tsec,Blog,网络安全,信息安全,安全运营,应急响应',
+  icon: 'http://pic.hackmyhome.top/i/202302020119787.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
   homePage: 'https://b.tsec.wiki',
   blogPage: 'https://tsec.wiki',
